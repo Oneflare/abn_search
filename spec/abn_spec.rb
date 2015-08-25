333ad34cad0da15a3de04e635d74ed12b61e2580
Stuart Auld <sauld@cozero.com.au>
2015-08-25 10:47:16 +1000

    tidy up the tests

--- spec/abn_spec.rb
+++ spec/abn_spec.rb
@@ -1,47 +1,81 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 
 describe ABNSearch::ABN do
-  describe "basic argument checking" do
-    it 'raises an error if the abn provided does not smell correct' do
-      expect{ ABNSearch::ABN.new('123')}.to raise_error(ArgumentError)
-      expect{ ABNSearch::ABN.new('123123123123')}.to raise_error(ArgumentError)
-    end
-
-    it 'returns an instance of an abn if everything looks right' do
-      expect( ABNSearch::ABN.new('99 124 391 073')).to be_a ABNSearch::ABN
-      expect( ABNSearch::ABN.new('99124391073')).to be_a ABNSearch::ABN
-      expect( ABNSearch::ABN.new('11111111111')).to be_a ABNSearch::ABN
-    end
-
-    it "should have a problem with invalid parameters" do
-      expect{ABNSearch::ABN.valid?(nil)}.to       raise_error(ArgumentError)
-      expect{ABNSearch::ABN.valid?(Array)}.to     raise_error(ArgumentError)
-      expect{ABNSearch::ABN.valid?(Array.new)}.to raise_error(ArgumentError)
-    end
-
-    it "should have a problem with invalid parameter type that has a #length of 11" do
-      bad_parameter = (1..11).to_a
-      expect{ABNSearch::ABN.valid?(bad_parameter)}.to raise_error(ArgumentError)
-    end
-
-  end
   describe "valid? class method" do
     it "should identify a valid ABN" do
-      expect(ABNSearch::ABN.valid?("12042168743")).to be true
-      expect(ABNSearch::ABN.valid?(12042168743)).to   be true
+      good_abns.each do |abn|
+        expect(abn.valid?).to be true
+      end
     end
 
-    it "should identify a preformatted valid ABN" do
-      expect(ABNSearch::ABN.valid?("12 042 168 743")).to be true
+    it "should identify an invalid ABN" do
+      bad_abns.each do |abn|
+        expect(abn.valid?).to be false
+      end
+    end
+  end
+
+  describe "valid_acn? class method" do
+    it "should identify a valid ACN" do
+      good_acns.each do |acn|
+        expect(acn.valid_acn?).to be true
+      end
     end
 
-    it "should have a problem with a pre-formatted invalid ABN" do
-      expect(ABNSearch::ABN.valid?("12 042 168 744")).to be false
+    it "should identify an invalid ACN" do
+      bad_acns.each do |acn|
+        expect(acn.valid_acn?).to be false
+      end
     end
+  end
 
-    it "should have a problem with an invalid ABN" do
-      expect(ABNSearch::ABN.valid?("12042168744")).to be false
+  describe ABNSearch::ABR do
+    describe "sanity check" do
+      it "should not attempt to query ABR without a GUID" do
+        expect{(good_abns.first.update_from_abr!)}.to raise_error(ArgumentError)
+      end
+
+      it "should fail if you use a fake GUID" do
+        ABNSearch::ABR.new('fake-guid')
+        expect{(good_abns.first.update_from_abr!)}.to raise_error(RuntimeError)
+      end
     end
+  end
+
+  def good_abns
+    [
+      ABNSearch::ABN.new({abn: 99124391073}),
+      ABNSearch::ABN.new({abn: '99124391073'}),
+      ABNSearch::ABN.new({abn: '99 12 439 10 73 '}),
+      ABNSearch::ABN.new({abn: '46 110 483 513'}),
+    ]
+  end
+
+  def bad_abns
+    [
+      ABNSearch::ABN.new({abn: 9912439107}),
+      ABNSearch::ABN.new({abn: 991243910711}),
+      ABNSearch::ABN.new({abn: 'tom'}),
+      ABNSearch::ABN.new({abn: '99124391072'}),
+    ]
+  end
+
+  def good_acns
+    [
+      ABNSearch::ABN.new({acn: 124391073}),
+      ABNSearch::ABN.new({acn: '124391073'}),
+      ABNSearch::ABN.new({acn: ' 12 439 10 73 '}),
+      ABNSearch::ABN.new({acn: '110 483 513'}),
+    ]
+  end
+
+  def bad_acns
+    [
+      ABNSearch::ABN.new({acn: 12439107}),
+      ABNSearch::ABN.new({acn: 1243910711}),
+      ABNSearch::ABN.new({acn: 'tom'}),
+      ABNSearch::ABN.new({acn: '124391072'}),
+    ]
   end
 
 end
