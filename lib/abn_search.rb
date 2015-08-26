eb280ad4b1fc16dd2cc7d2a13b76fb11c8a21b65
Stuart Auld <sauld@cozero.com.au>
2015-08-27 09:40:04 +1000

    gemspec and version tidy

--- lib/abn_search.rb
+++ lib/abn_search.rb
@@ -1,5 +1,6 @@
 require 'abn_search/abn.rb'
 require 'abn_search/abr.rb'
+require 'abn_search/version.rb'
 
 # ABNSearch module to encapsulate all classes
 module ABNSearch
--- lib/abn_search/version.rb
+++ lib/abn_search/version.rb
@@ -1,3 +1,6 @@
 module ABNSearch
-  VERSION = '1.0.0'
+  MAJOR = 1
+  MINOR = 0
+  PATCH = 0
+  VERSION = [MAJOR, MINOR, PATCH].join('.')
 end
